85dbfa011e5e213700bf2133d1cf9b043d84cd02
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-13 22:44:58 +0900

    upload btvn ss11-2

---
+++ ss11-2.js
@@ -0,0 +1,7 @@
+let x = prompt("What is the official name of JavaScript?");
+if (x === "ECMAScript"){
+    alert("Right!");
+}
+else if (x !== "ECMAScript"){
+    alert("Didn’t know? ECMAScript");
+}
