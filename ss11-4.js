271df60234f2923d0f8fb0b2d6efa7cc40344f7a
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-13 23:34:54 +0900

    upload btvn ss11-4

---
+++ ss11-4.js
@@ -0,0 +1,20 @@
+let x = prompt("hãy nhập một số bất kì từ 1 đến 9");
+if(x==="1"){
+    alert("số một");
+}else if(x==="2"){
+    alert("số hai");
+}else if(x==="3"){
+    alert("số ba");
+}else if(x==="4"){
+    alert("số bốn");
+}else if(x==="5"){
+    alert("số năm");
+}else if(x==="6"){
+    alert("số sáu");
+}else if(x==="7"){
+    alert("số bảy");
+}else if(x==="8"){
+    alert("số tám");
+}else if(x==="9"){
+    alert("số chín");
+}
