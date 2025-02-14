b24384fb9ade4c8ddf04b010fddb607222708874
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-14 23:59:34 +0900

    upload btvn ss11-1

--- ss11-1.js
+++ ss11-1.js
@@ -1,4 +1,4 @@
-let x = prompt("hãy nhập năm mà bạn muốn");
+let x = +prompt("hãy nhập năm mà bạn muốn");
 
 if (Number.isInteger(x/4) && Number.isFinite(x/100)) {
         alert ("đây là năm nhuận");
