05e2eff7ce84875f4dc966702405387cabc9f324
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-14 23:57:08 +0900

    upload lại file ss11-5.js

--- ss11-5.js
+++ ss11-5.js
@@ -1,4 +1,9 @@
-let x = prompt("The number is N");
-for (x=0; x <=5; x=x+1 ){
-    alert(x)
+let i = prompt("The number is N");
+for (i=1; i <=5; i=i+1 ){
+    console.log(0);
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
 }
